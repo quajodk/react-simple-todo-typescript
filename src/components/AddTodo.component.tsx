6d6e9b391c57bcc305680f02ac93527977bd4397
quajoduke <quajoduke305@gmail.com>
2019-11-18 20:55:08 +0000

    todo components created

---
+++ src/components/AddTodo.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddTodo: React.FC = () => {
+  return <div></div>;
+};
+
+export default AddTodo;
---
+++ src/components/EditTodo.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditTodo: React.FC = () => {
+  return <div></div>;
+};
+
+export default EditTodo;
